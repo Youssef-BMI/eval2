0f0b5cc486df835ac2a79536b2d4faddd1760d5a
Youssef-BMI <sarfouhiyoussef@gmail.com>
2023-12-15 13:42:18 +0000

    Récupération du choix de l'utilisateur

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,40 @@ int main () {
   int nbrePlaces = 10;
   int choix;
   std::cout << "************ Evaluation 2 ************\n";
+  std::cout << " \n";
+  std::cout << "    1 - Entrée dans le parking\n";
+  std::cout << "    2 - Nombres de places disponibles\n";
+  std::cout << "    3 - Sortie du parking\n";
+  std::cout << "    4 - Quitter\n";
+  std::cout << " \n";
+  std::cout << "**************************************\n";
+  std::cin >> choix ;
+  std::cout << ": choix : \n" << choix ;
+  switch (choix) 
+    {
+      case 1 :
+        std::cout << " - Entrée dans le parking \n";
+        break ;
+
+      case 2 :
+        std::cout << " - Nombres de places disponibles \n"; 
+        break ;
+
+      case 3 : 
+        std::cout << " - Sortie du parking\n";
+        break ;
+
+      case 4 :
+        std::cout << " - Quitter\n"; 
+        break ;
 
 
+  default:
+       std::cout << " CHOIX INCORECTE\n" ; 
+    }
+  
+  
+  
+  
   return 0;
 }
